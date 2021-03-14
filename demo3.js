113237c1cab9299b794cd9d3e3dab0467d23a771
chrisapeh02 <80574050+chrisapeh02@users.noreply.github.com>
2021-03-14 16:56:49 +0100

    Update demo3.js

--- demo3.js
+++ demo3.js
@@ -3,8 +3,8 @@ const log = console.log;
 const api = require('binance');
 
 const brest = new api.BinanceRest({
-    key: 'Lh4Oo7vlglF18ms5mOpgtAdGl2gfp0xYtwQkApvaYMBcimkKMqMjufQ7suEtDOpx', // Get this from your account on binance.com
-    secret: 'MXnIkrMCHDF37yGKbEujdnTABxxsLoUwvPmr0fYXSO0buAyEeVzRLLVEYkKQo9cU', // Same for this
+    key: 'Gjg6uiLKgpaQqDs9daEw26FvhmCFThDYHXdSqf0wiSJbr1Wv4LHzdcnZiqryGA1U', // Get this from your account on binance.com
+    secret: 'UH0vui1Zi2aUKjgUdDb0ryI1ncN7sGjQ3aZk5YxWwB84NmwGElb223hLTA69moZ6', // Same for this
     timeout: 15000, // Optional, defaults to 15000, is the request time out in milliseconds
     recvWindow: 20000, // Optional, defaults to 5000, increase if you're getting timestamp errors
     disableBeautification: false,
